e626023528cd23e4c0b985ac2ea3e479d23a9aa3
Yevhen Shymotiuk <yshym@pm.me>
2022-06-21 16:02:06 +0300

    flake: add overlay and nixos module

--- flake.nix
+++ flake.nix
@@ -11,27 +11,30 @@
       inherit (flake-utils.lib) eachSystem;
 
       supportedSystems = [ "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
     in
-    eachSystem
-      supportedSystems
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          devShell = with pkgs;
-            mkShell {
-              nativeBuildInputs = [ python37 poetry ];
-            };
-          defaultPackage = with pkgs.python3Packages; buildPythonPackage {
-            pname = "clion";
-            version = "0.3.2";
-            format = "pyproject";
+    {
+      overlay = final: prev: {
+        clion = with prev.python3Packages; buildPythonPackage {
+          pname = "clion";
+          version = "0.3.2";
+          format = "pyproject";
 
-            disabled = pythonOlder "3.7";
+          disabled = pythonOlder "3.7";
 
-            buildInputs = [ poetry ];
+          buildInputs = [ poetry ];
 
-            src = self;
-          };
-        });
+          src = self;
+        };
+      };
+
+      defaultPackage = forAllSystems (system: (import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      }).clion);
+
+      nixosModules.clion = { pkgs, ... }: {
+        nixpkgs.overlays = [ self.overlay ];
+      };
+    };
 }
