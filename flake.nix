2bbd84d06f838f1a9b41d6875997713efd2ff797
Yevhen Shymotiuk <yshym@pm.me>
2022-06-21 14:41:37 +0300

    flake: add defaultPackage

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Clinic";
+  description = "Clion";
 
   inputs = {
     nixpkgs.url = "github:yevhenshymotiuk/nixpkgs/release-21.11";
@@ -7,13 +7,31 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShell = with pkgs;
-          mkShell {
-            nativeBuildInputs = [ python37 poetry ];
+    let
+      inherit (flake-utils.lib) eachSystem;
+
+      supportedSystems = [ "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
+    in
+    eachSystem
+      supportedSystems
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          devShell = with pkgs;
+            mkShell {
+              nativeBuildInputs = [ python37 poetry ];
+            };
+          defaultPackage = with pkgs.python3Packages; buildPythonPackage {
+            pname = "clion";
+            version = "0.3.2";
+            format = "pyproject";
+
+            disabled = pythonOlder "3.7";
+
+            buildInputs = [ poetry ];
+
+            src = self;
           };
-      });
+        });
 }
